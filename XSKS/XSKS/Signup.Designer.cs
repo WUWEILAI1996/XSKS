b2ba4536a099e0288752170f23de80ab718e90f1
wwl <171420183@qq.com>
2017-06-05 09:01:46 +0800

    17/6/4-2

--- XSKS/XSKS/Signup.Designer.cs
+++ XSKS/XSKS/Signup.Designer.cs
@@ -35,19 +35,18 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("宋体", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(165, 49);
+            this.label1.Location = new System.Drawing.Point(162, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(136, 56);
             this.label1.TabIndex = 0;
@@ -57,17 +56,17 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(79, 156);
+            this.label2.Location = new System.Drawing.Point(58, 163);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 21);
+            this.label2.Size = new System.Drawing.Size(73, 21);
             this.label2.TabIndex = 1;
-            this.label2.Text = "姓名";
+            this.label2.Text = "用户名";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label3.Location = new System.Drawing.Point(79, 201);
+            this.label3.Location = new System.Drawing.Point(79, 204);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(52, 21);
             this.label3.TabIndex = 2;
@@ -77,7 +76,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label4.Location = new System.Drawing.Point(79, 248);
+            this.label4.Location = new System.Drawing.Point(37, 253);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(94, 21);
             this.label4.TabIndex = 3;
@@ -86,80 +85,81 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox1.Location = new System.Drawing.Point(210, 153);
+            this.textBox1.Location = new System.Drawing.Point(156, 156);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(169, 31);
-            this.textBox1.TabIndex = 4;
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox2.Location = new System.Drawing.Point(210, 198);
+            this.textBox2.Location = new System.Drawing.Point(156, 201);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(169, 31);
-            this.textBox2.TabIndex = 5;
+            this.textBox2.TabIndex = 3;
+            this.textBox2.UseSystemPasswordChar = true;
             // 
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBox3.Location = new System.Drawing.Point(210, 238);
+            this.textBox3.Location = new System.Drawing.Point(156, 248);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(169, 31);
-            this.textBox3.TabIndex = 6;
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.radioButton2);
-            this.panel1.Controls.Add(this.radioButton1);
-            this.panel1.Location = new System.Drawing.Point(83, 294);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(296, 47);
-            this.panel1.TabIndex = 7;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.radioButton2.Location = new System.Drawing.Point(193, 14);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(58, 20);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "考生";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.radioButton1.Location = new System.Drawing.Point(32, 14);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(74, 20);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "管理员";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.textBox3.TabIndex = 4;
+            this.textBox3.UseSystemPasswordChar = true;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button1.Location = new System.Drawing.Point(101, 388);
+            this.button1.Location = new System.Drawing.Point(83, 350);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(108, 37);
             this.button1.TabIndex = 8;
             this.button1.Text = "注册";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button2.Location = new System.Drawing.Point(253, 388);
+            this.button2.Location = new System.Drawing.Point(254, 350);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(108, 37);
             this.button2.TabIndex = 9;
             this.button2.Text = "退出";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("宋体", 10F);
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(331, 166);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 14);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(79, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 21);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "姓名";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox4.Location = new System.Drawing.Point(156, 109);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(169, 31);
+            this.textBox4.TabIndex = 1;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // Signup
             // 
@@ -168,12 +168,14 @@
             this.ClientSize = new System.Drawing.Size(484, 461);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.MaximumSize = new System.Drawing.Size(500, 500);
@@ -181,8 +183,7 @@
             this.Name = "Signup";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Signup";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.Load += new System.EventHandler(this.Signup_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -197,10 +198,10 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
--- XSKS/XSKS/Signup.cs
+++ XSKS/XSKS/Signup.cs
@@ -6,11 +6,15 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace XSKS
 {
+  
     public partial class Signup : Form
     {
+        SqlConnection Mycon;
         public Signup()
         {
             InitializeComponent();
@@ -30,5 +34,73 @@ namespace XSKS
                 return cp;
             }
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            SqlDataAdapter da;
+            DataSet ds = new DataSet();
+            string connstr = ConfigurationManager.ConnectionStrings["SQLConnString"].ConnectionString;
+            Mycon = new SqlConnection(connstr);
+            Mycon.Open();
+
+            SqlCommand search = new SqlCommand("select * from Stu where id = '"+textBox1.Text+"'", Mycon);
+            da = new SqlDataAdapter(search);
+            da.Fill(ds, "Stu");
+            if (textBox1.Text != "")
+            {
+                if (ds.Tables["Stu"].Rows.Count > 0)
+                {
+                    label6.Text = "此用户名已经存在！";
+                    label6.ForeColor = Color.Red;
+                }
+                else
+                {
+                    label6.Text = "此用户名可以使用！";
+                    label6.ForeColor = Color.Green;
+                }
+            }
+            else
+                label6.Text = "";
+        }
+
+        private void Signup_Load(object sender, EventArgs e)
+        {
+            label6.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter da;
+            DataSet ds = new DataSet();
+            string connstr = ConfigurationManager.ConnectionStrings["SQLConnString"].ConnectionString;
+            Mycon = new SqlConnection(connstr);
+            Mycon.Open();
+
+            if (textBox4.Text == "")
+            {
+                MessageBox.Show("姓名不能为空！");
+            }
+            else if (textBox1.Text == "")
+            {
+                MessageBox.Show("用户名不能为空！");
+            }
+            else if (textBox2.Text == "")
+            {
+                MessageBox.Show("密码不能为空！");
+            }
+            else if (textBox3.Text == "")
+            {
+                MessageBox.Show("请确认密码！");
+            }
+            else if (textBox2.Text != textBox3.Text)
+            {
+                MessageBox.Show("两处密码不同");
+            }
+            else
+            {
+                SqlCommand insert = new SqlCommand("INSERT INTO Stu VALUES ('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox4.Text + "','')", Mycon);
+            }
+        }
+
     }
 }
